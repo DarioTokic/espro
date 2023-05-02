72f8bedee037677c4ceb8e35929e1548ab25a377
Xabaras666 <dariotokicjajce@gmail.com>
2023-05-02 14:15:40 +0200

    made bestseller section

--- espro/src/Components/BestSellers/ItemCards/Card/Card.js
+++ espro/src/Components/BestSellers/ItemCards/Card/Card.js
@@ -2,17 +2,70 @@ import React from 'react';
 
 import './Card.css';
 
+import star from '../../../../assets/star.svg';
+import starEmpty from '../../../../assets/starEmpty.svg';
+
 const card = (props) => (
     <div className='Card'>
-        <img src={props.img} />
-        <div className='stars'>
-            <p>dodati zvjezde</p>
+        <img src={props.image} />
+        <div className='description'>
+            {props.stars == '5' ?
+                <div className='stars'>
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                </div>
+            :props.stars == '4' ?
+                <div className='stars'>
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={starEmpty} />
+                </div>
+            :props.stars == '3' ?
+                <div className='stars'>
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                </div>
+            :props.stars == '2' ?
+                <div className='stars'>
+                    <img src={star} />
+                    <img src={star} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                </div>
+            :props.stars == '1' ?
+                <div className='stars'>
+                    <img src={star} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                </div>
+            :   <div className='stars'>
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                    <img src={starEmpty} />
+                </div>
+            }
+            <h1>{props.title}</h1>
+            <p>${props.startingPrice} - ${props.endingPrice}</p>
+            <div className='colours'>
+                <div className='grayProduct'></div>
+                <div className='whiteProduct'></div>
+                <div className='blackProduct'></div>
+            </div>
         </div>
-        <h1>{props.title}</h1>
-        <p>${props.startingPrice} - ${props.endingPrice}</p>
-        <div className='colours'>
-            <p>dodati boje</p>
-        </div>
+        
     </div>
 );
 
--- espro/src/Components/BestSellers/ItemCards/ItemCards.js
+++ espro/src/Components/BestSellers/ItemCards/ItemCards.js
@@ -4,9 +4,20 @@ import './ItemCards.css';
 
 import Card from './Card/Card';
 
+import espro1 from '../../../assets/espro1.webp'
+import espro2 from '../../../assets/espro2.webp'
+
 const itemCards = (props) => (
     <div className='ItemCards'>
-        <Card image={} title={} startingPrice={} endingPrice={} />
+        <Card image={espro1} stars='5' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
+        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' startingPrice='109.95' endingPrice='139.95' />
     </div>
 );
 
