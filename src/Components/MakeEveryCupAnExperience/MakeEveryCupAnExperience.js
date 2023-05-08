b563a404986fef7c93dd8d6686f99e90af448e8c
Xabaras666 <dariotokicjajce@gmail.com>
2023-05-08 13:00:18 +0200

    added carousel to makeEveryCupAnExperience sectoin

--- src/Components/MakeEveryCupAnExperience/MakeEveryCupAnExperience.js
+++ src/Components/MakeEveryCupAnExperience/MakeEveryCupAnExperience.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './MakeEveryCupAnExperience.css';
 
@@ -10,52 +10,81 @@ import img5 from '../../assets/section4_5.png';
 
 import Dot from '../Dot/Dot';
 
-const makeEveryCupAnExperience = (props) => (
-    <div className='MakeEveryCupAnExperience'>
-        <div 
-            className='left'
-            style={{backgroundImage: `url(${img1})`}}
-        >
-            <h1>MAKE EVERY CUP AN EXPERIENCE.</h1>
-            <p>
-                Have your French pressed coffee anywhere with ESPRO’s 
-                Travel Press. A double micro-filter eliminates grit for 
-                your smoothest cup on the go, and the mug fits right in 
-                a car cup holder or bike’s water bottle cage.
-            </p>
-            <button>SHOP TRAVEL PRESS</button>
-           
-        </div>
-        <div className='right'>
-            <img src={img2} alt='img2' />
-            <h1>PRESS. SIP. ENJOY.</h1>
-        </div>
-        <div className='dots'>
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-            <Dot colour='#fff' />
-        </div>
-    </div>
-);
+const MakeEveryCupAnExperience = (props) => {
 
-export default makeEveryCupAnExperience;
+    useEffect(() => {
+
+        var imageIndex = 0;
+        
+        const carousel = () => {
+            const images = document.querySelectorAll('.slides');
+            images.forEach((image) => {
+                        image.style.display = 'none';
+            });
+
+            imageIndex++;
+
+            if (imageIndex > images.length) { imageIndex = 1; }
+            images[imageIndex - 1].style.display = 'block';
+            setTimeout(carousel, 2000);
+        }
+        
+
+        carousel();
+    })
+
+    return (
+        <div className='MakeEveryCupAnExperience'>
+            <div 
+                className='left'
+                style={{backgroundImage: `url(${img1})`}}
+            >
+                <h1>MAKE EVERY CUP AN EXPERIENCE.</h1>
+                <p>
+                    Have your French pressed coffee anywhere with ESPRO’s 
+                    Travel Press. A double micro-filter eliminates grit for 
+                    your smoothest cup on the go, and the mug fits right in 
+                    a car cup holder or bike’s water bottle cage.
+                </p>
+                <button>SHOP TRAVEL PRESS</button>
+               
+            </div>
+            <div className='right'>
+                <div className='carousel'>
+                    <img className='img2 slides' src={img2} alt='img2' />
+                    <img className='img3 slides' src={img3} alt='img3' />
+                    <img className='img4 slides' src={img4} alt='img4' />
+                    <img className='img5 slides' src={img5} alt='img5' />
+                </div>
+                <h1>PRESS. SIP. ENJOY.</h1>
+            </div>
+            <div className='dots'>
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+                <Dot colour='#fff' />
+            </div>
+        </div>
+    );
+}; 
+
+export default MakeEveryCupAnExperience;
