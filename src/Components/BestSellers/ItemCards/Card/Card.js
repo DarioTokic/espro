9c550aa7a57c03273d6a0a3212d59a045044f069
Xabaras666 <dariotokicjajce@gmail.com>
2023-05-10 14:17:38 +0200

    fixed bestSellers section

--- src/Components/BestSellers/ItemCards/Card/Card.js
+++ src/Components/BestSellers/ItemCards/Card/Card.js
@@ -8,62 +8,52 @@ import starEmpty from '../../../../assets/starEmpty.svg';
 const card = (props) => (
     <div className='Card'>
         <img src={props.image} alt='productImage' />
-        <div className='description'>
-            {props.stars === '5' ?
-                <div className='stars'>
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                </div>
-            :props.stars === '4' ?
-                <div className='stars'>
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={starEmpty} alt='starEmpty' />
-                </div>
-            :props.stars === '3' ?
-                <div className='stars'>
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                </div>
-            :props.stars === '2' ?
-                <div className='stars'>
-                    <img src={star} alt='star' />
-                    <img src={star} alt='star' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                </div>
-            :props.stars === '1' ?
-                <div className='stars'>
-                    <img src={star} alt='star' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                </div>
-            :   <div className='stars'>
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                    <img src={starEmpty} alt='starEmpty' />
-                </div>
-            }
-            <h1 className='prodTitle'>{props.title}</h1>
-            <p className='prodPrice'>{props.price}</p>
-            <div className='colours'>
-                <div className='grayProduct'></div>
-                <div className='whiteProduct'></div>
-                <div className='blackProduct'></div>
+        { props.label ?
+            <div 
+                className='label' 
+                style={{backgroundColor: 
+                    props.label === "SALE" ? 
+                    "#F24343" : 
+                    props.label === "NEW!" ? 
+                    "#6BBBAE" : 
+                    props.label === "BACK IN STOCK" ? 
+                    "#8DB9CA" : 
+                    null }}
+            >
+                <p>{props.label}</p>
             </div>
+            : null
+        }
+        
+        <div className='description'>
+        <div className='stars'>
+            {[1, 2, 3, 4, 5].map((index) => (
+                <img
+                key={index}
+                src={index <= props.stars ? star : starEmpty}
+                alt={index <= props.stars ? 'star' : 'starEmpty'}
+                />
+            ))}
+        </div>
+        <h1 className='prodTitle'>{props.name}</h1>
+        <div className='prodPrice'>
+            <p>{props.price}</p>
+            <span><s>{props.discountedPrice}</s></span>
+        </div>
+        <div className='colors'>
+            {
+                props.colors.map((color, index) => <div 
+                                            className={`color ${index === 0 ? 'active' : ''}`}
+                                            style={{backgroundColor: color}} 
+                                            onClick={(e) => {
+                                                const parent = e.target.parentNode;
+                                                const colors = parent.querySelectorAll('.color');
+                                                colors.forEach(color => color.classList.remove('active'));
+                                                e.target.classList.add('active')
+                                            }}  
+                                        />)
+            }
+        </div>
         </div>
         
     </div>
--- src/Components/BestSellers/ItemCards/ItemCards.js
+++ src/Components/BestSellers/ItemCards/ItemCards.js
@@ -3,16 +3,14 @@ import React from 'react';
 import './ItemCards.css';
 
 import Card from './Card/Card';
-
-import espro1 from '../../../assets/espro1.webp'
-import espro2 from '../../../assets/espro2.webp'
-import espro3 from '../../../assets/espro3.png'
+import items from './items.json';
 
 const itemCards = (props) => (
     <div className='ItemCards'>
-        <Card image={espro1} stars='5' title='ESPRO COFFEE FRENCH PRESS P7' price='$109.95 - $139.95' colors={('#F6F6F6', '#FFFFFF', '#000000')} />
-        <Card image={espro2} stars='4' title='ESPRO COFFEE FRENCH PRESS P7' price='$89.95' colors={('#F6F6F6', '#000000')} />
-        <Card image={espro3} stars='5' title='BLOOM POUR OVER COFFEE BREWING KIT, 18 OZ' price='$99.95' />
+
+        {items.map(item => (
+            <Card key={item.id} name={item.name} image={item.image} stars={item.stars} price={item.price} discountedPrice={item.discountedPrice} colors={item.colors} label={item.label} />
+        ))}
     </div>
 );
 
